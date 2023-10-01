1998a243df920c36e09f171365308c0f365e016b
TrinhLV <vtrinh111@gmail.com>
2023-10-01 14:35:14 +0700

    fix cmt

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,26 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
-# Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
-
-# Step 3:
-# Push image to a docker repository
-#!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
-
-# Assumes that an image is built via `run_docker.sh`
-
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
 dockerpath=vtrinh111/devopsproject4
 tag=1.0
 
 # Step 2:  
 # Authenticate & tag
+echo "Docker login:"
+docker login
+
 echo "Docker ID and Image: $dockerpath tag: $tag"
 docker tag $dockerpath $dockerpath:$tag
 
